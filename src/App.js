81c8c41e01cf2d0d6d4d85f4e93f98c702a65c37
Vijay <jayagrawal.9140@gmail.com>
2022-09-26 22:16:45 +0530

    error handle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+
 import Page from './component/Page';
 function App() {
   return (
     <div className="">
      <Page/>
 
-    
+    {/* <Api/> */}
       
     </div>
   );
--- src/component/Page.js
+++ src/component/Page.js
@@ -4,31 +4,29 @@ import './page.css';
 
 const Page = () => {
   // const{email,first_name,last_name,avatar} = props.users;
-  const [id, setId] = useState(1);
+  const [id, setId] = useState();
   const [list, setList] = useState([]);
   const [loading, setLaoding] = useState(false);
   const [error, setError] = useState(false);
   const [msg, setMessage] = useState("");
+  const[click,setClick] = useState(false);
 
-  function changehand(e) {
-    setId(e.target.value);
+  function handler(e){
+      e.preventDefault();
+      setClick(true);
 
-  }
-
-  useEffect(() => {
-    setLaoding(true);
-
-    fetch(`https://reqres.in/api/users/${id}`)
+      fetch(`https://reqres.in/api/users/${id}`)
       .then((res) => {
-        //console.log(res.json());
-        if (res.status >= 400) {
-          throw new Error("Server responds with error!")
-        }
+       // console.log(res.json());
+       if (res.status >= 400) {
+        throw new Error("Server responds with error!")
+      }
         res.json()
           .then((result) => {
             setList(result.data)
             setError(false);
             setLaoding(false);
+           console.log(result)
           })
 
 
@@ -40,14 +38,38 @@ const Page = () => {
         setMessage(err.message);
 
       })
+    }
 
-  }, [id])
+  
   return (
     <>
+
+
       <div className="main-content">
+      
         {
-          error && loading ? <h3 style={{ color: 'red', marginTop: '7rem' }}>{msg}</h3>
+          
+          click ?
+          error && loading ? 
+          <div className='right'>
+          <input type='number'
+            value={id}
+            onChange={
+              (e) => {
+                setId(e.target.value);
+                if(e.target.value == ""){
+                  setClick(false)
+                }
+              }
+            }
+
+          />
+         
+         <button disabled={!id} onClick={handler}>Submit</button>
+          <h3 style={{ color: 'red', marginTop: '7rem' }}>{msg}</h3>
+          </div>
             :
+            <div className="main-content">
             <div className='left'>
               <img src={list.avatar} alt='profile_pic' />
             
@@ -63,18 +85,42 @@ const Page = () => {
               <p>This is the <span style={{ color: 'red' }}>{list.first_name} {list.last_name}</span> profiles</p>
               
             </div>
-        }
-        <div className='right'>
+            <div className='right'>
           <h1>Search User Profile</h1>
           <input type='number'
             value={id}
             onChange={
-              (e) => setId(e.target.value)
-
+              (e) => {
+                setId(e.target.value);
+              }
             }
 
           />
+         
+         <button disabled={!id} onClick={handler}>Submit</button>
         </div>
+              </div>
+            :
+            <div className='right'>
+          <h1>Search User Profile</h1>
+          <input type='number'
+            value={id}
+            onChange={
+              (e) => {
+                setId(e.target.value);
+                if(e.target.value == ""){
+                  setClick(false)
+                }
+              }
+            }
+
+          />
+         
+         <button disabled={!id} onClick={handler}>Submit</button>
+        </div>
+  
+        }
+       
       </div>
     </>
   )

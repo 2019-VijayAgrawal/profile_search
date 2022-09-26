524b23ac68fc278c58098440cbaa1a7a39457424
Vijay <jayagrawal.9140@gmail.com>
2022-09-26 23:04:27 +0530

    done

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import './page.css';
-
+import React, { useEffect, useState } from "react";
+import "./page.css";
 
 const Page = () => {
   // const{email,first_name,last_name,avatar} = props.users;
@@ -9,121 +8,123 @@ const Page = () => {
   const [loading, setLaoding] = useState(false);
   const [error, setError] = useState(false);
   const [msg, setMessage] = useState("");
-  const[click,setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-  function handler(e){
-      e.preventDefault();
-      setClick(true);
+  function handler(e) {
+    e.preventDefault();
+    setClick(true);
 
-      fetch(`https://reqres.in/api/users/${id}`)
+    fetch(`https://reqres.in/api/users/${id}`)
       .then((res) => {
-       // console.log(res.json());
-       if (res.status >= 400) {
-        throw new Error("Server responds with error!")
-      }
-        res.json()
-          .then((result) => {
-            setList(result.data)
-            setError(false);
-            setLaoding(false);
-           console.log(result)
-          })
-
-
+        // console.log(res.json());
+        if (res.status >= 400) {
+          throw new Error("Server responds with error!");
+        }
+        res.json().then((result) => {
+          setList(result.data);
+          setError(false);
+          setLaoding(false);
+          console.log(result);
+        });
       })
       .catch((err) => {
-        console.log(err.message)
+        console.log(err.message);
         setError(true);
         setLaoding(true);
         setMessage(err.message);
+      });
+  }
 
-      })
-    }
-
-  
   return (
     <>
-
-
       <div className="main-content">
-      
-        {
-          
-          click ?
-          error && loading ? 
-          <div className='right'>
-          <input type='number'
-            value={id}
-            onChange={
-              (e) => {
-                setId(e.target.value);
-                if(e.target.value == ""){
-                  setClick(false)
-                }
-              }
-            }
+        {click ? (
+          error && loading ? (
+            <div className="right">
+              <input
+                type="number"
+                value={id}
+                onChange={(e) => {
+                  setId(e.target.value);
+                  if (e.target.value == "") {
+                    setClick(false);
+                    setId("");
+                  }
+                }}
+              />
 
-          />
-         
-         <button disabled={!id} onClick={handler}>Submit</button>
-          <h3 style={{ color: 'red', marginTop: '7rem' }}>{msg}</h3>
-          </div>
-            :
-            <div className="main-content">
-            <div className='left'>
-              <img src={list.avatar} alt='profile_pic' />
-            
-              <div className='line'></div>
-              <h2 style={{ color: 'yellow' }}>Name </h2>
-              <h3>{list.first_name} {list.last_name}</h3>
-              <div className='line'></div>
-              <br/>
-              <h2 style={{ color: 'yellow' }}>Email</h2>
-              <h4> {list.email}</h4>
-              <div className='line'></div>
-              <h2 style={{ color: 'yellow' }}>Description :</h2>
-              <p>This is the <span style={{ color: 'red' }}>{list.first_name} {list.last_name}</span> profiles</p>
-              
+              <button disabled={!id} onClick={handler}>
+                Submit
+              </button>
+              <h3 style={{ color: "red", marginTop: "7rem" }}>{msg}</h3>
             </div>
-            <div className='right'>
-          <h1>Search User Profile</h1>
-          <input type='number'
-            value={id}
-            onChange={
-              (e) => {
-                setId(e.target.value);
-              }
-            }
+          ) : (
+            <div className="main-content">
+              <div className="left">
+                <img src={list.avatar} alt="profile_pic" />
 
-          />
-         
-         <button disabled={!id} onClick={handler}>Submit</button>
-        </div>
+                <div className="line"></div>
+                <h2 style={{ color: "yellow" }}>Name </h2>
+                <h3>
+                  {list.first_name} {list.last_name}
+                </h3>
+                <div className="line"></div>
+                <br />
+                <h2 style={{ color: "yellow" }}>Email</h2>
+                <h4> {list.email}</h4>
+                <div className="line"></div>
+                <h2 style={{ color: "yellow" }}>Description :</h2>
+                <p>
+                  This is the{" "}
+                  <span style={{ color: "red" }}>
+                    {list.first_name} {list.last_name}
+                  </span>{" "}
+                  profiles
+                </p>
               </div>
-            :
-            <div className='right'>
-          <h1>Search User Profile</h1>
-          <input type='number'
-            value={id}
-            onChange={
-              (e) => {
-                setId(e.target.value);
-                if(e.target.value == ""){
-                  setClick(false)
+              <div className="right">
+                <h1>Search User Profile</h1>
+                <input
+                  type="number"
+                  value={id}
+                  onChange={(e) => {
+                    setId(e.target.value);
+                    if (e.target.value === "") {
+                  setClick(false);
+                  setId("");
                 }
-              }
-            }
+                  }}
+                />
 
-          />
-         
-         <button disabled={!id} onClick={handler}>Submit</button>
-        </div>
-  
-        }
-       
+                <button disabled={!id} onClick={handler}>
+                  Submit
+                </button>
+              </div>
+            </div>
+          )
+        ) : (
+          <div className="right">
+            <h1>Search User Profile</h1>
+            <input
+              type="number"
+              value={id}
+              onChange={(e) => {
+                setId(e.target.value);
+                if (e.target.value === "") {
+                  setClick(false);
+                  setId("");
+                }
+              }}
+            />
+
+            <button disabled={!id} onClick={handler}>
+              Submit
+            </button>
+          </div>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Page;
